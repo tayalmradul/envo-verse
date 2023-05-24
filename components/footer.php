6db4aa2b8035606f838c2d335814774d6bb01bd4
tayalmradul <116020480+tayalmradul@users.noreply.github.com>
2023-05-24 22:10:37 +0530

    sustainable e commerce

---
+++ components/footer.php
@@ -0,0 +1,25 @@
+<footer class="footer">
+
+   <section class="grid">
+
+      <div class="box">
+         <h3>contact us</h3>
+         <a href=""><i class="fas fa-phone"></i> +91 76696xxxxx</a>
+         <a href=""><i class="fas fa-phone"></i> +91 987564xxxx</a>
+         <a href=""><i class="fas fa-envelope"></i> envoverse123.com</a>
+         <a href="https://www.google.com/myplace"><i class="fas fa-map-marker-alt"></i> Uttar prdesh</a>
+      </div>
+
+      <div class="box">
+         <h3>follow us</h3>
+         <a href="#"><i class="fab fa-facebook-f"></i>facebook</a>
+         <a href="#"><i class="fab fa-twitter"></i>twitter</a>
+         <a href="#"><i class="fab fa-instagram"></i>instagram</a>
+         <a href="#"><i class="fab fa-linkedin"></i>linkedin</a>
+      </div>
+
+   </section>
+
+   <div class="credit"><?= date('Y'); ?> by <span>team Envo verse</span> ❤️</div>
+
+</footer>
